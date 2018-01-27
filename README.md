40b3f6cb7758a87b776f154b667980fed30e8bde
2BoysAndHats <conorb@firstpartners.net>
2018-01-27 12:43:15 +0000

    Init commit:
           * layout
           * blank moves controller

---
+++ README.md
@@ -0,0 +1,3 @@
+# TheExtraMile
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/MovesController.js
@@ -0,0 +1,20 @@
+/**
+ * MovesController
+ *
+ * @description :: Server-side logic for managing Moves
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    signup: function (req,res) {
+        
+    },
+
+    movesCallback: function (req,res) {
+
+    },
+
+    finishSignup: function (req,res) {
+
+    }
+};
